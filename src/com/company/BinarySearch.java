b7542819f3cfbbe78d5bb63e1ae56cab20e71764
Jesus Ortega del Castillo <52591802+1JesusOrtegadelCastillo@users.noreply.github.com>
2021-08-05 19:17:51 -0700

    Update BinarySearch.java

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class BinarySearch {
 
     public void binarySearch(int arr[], int num){
-        int low = 0;
-        int high = arr.length - 1;
+        int low = 0; // pointer starting at position 0
+        int high = arr.length - 1; // pointer starting at last position
 
         while (low <= high){
-            int mid = (low + high)/2;
+            int mid = (low + high)/2; // middle element
             int guess = arr[mid];
-            if (guess == num){
+            if (guess == num){ // if element has been found
                 System.out.println("Number: " + num + " ... found at position: " + mid);
                 break;
-            }else if(guess > num){
-                high = mid - 1;
+            }else if(guess > num){ // if number is too high
+                high = mid - 1; 
             } else {
-                low = mid + 1;
+                low = mid + 1; // if number is too low 
             }
-            if (low > high) {
+            if (low > high) { // if the element is not found
                 System.out.printf("Number: " + num + " ... not found");
             }
         }
